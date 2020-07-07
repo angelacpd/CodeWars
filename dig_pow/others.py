2149b7dd8874c6ff7c7fb8d14739eef611fcefd7
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-07-06 22:49:42 -0300

    dig_pow - format

--- dig_pow/others.py
+++ dig_pow/others.py
@@ -1,14 +1,15 @@
 def dig_pow(n, p):
-  s = 0
-  for i,c in enumerate(str(n)):
-     s += pow(int(c),p+i)
-  return s/n if s%n==0 else -1
+    s = 0
+    for i, c in enumerate(str(n)):
+        s += pow(int(c), p+i)
+    return s/n if s % n == 0 else -1
 
 
 def dig_pow(n, p):
     k, fail = divmod(sum(int(d)**(p + i) for i, d in enumerate(str(n))), n)
     return -1 if fail else k
 
+
 def dig_pow(n, p):
-  t = sum( int(d) ** (p+i) for i, d in enumerate(str(n)) )
-  return t//n if t%n==0 else -1
+    t = sum(int(d) ** (p+i) for i, d in enumerate(str(n)))
+    return t//n if t % n == 0 else -1
